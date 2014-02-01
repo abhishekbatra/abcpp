6b6d4d92384484767a8f2cf88998a9f394a38da1
Abhishek Batra <abhshk.btra@gmail.com>
2014-02-01 17:32:35 +0530

    Updates to Graph classes

--- graph.cpp
+++ graph.cpp
@@ -1,31 +1,60 @@
 #include "graph.h"
 #include <cstddef>
 #include <queue>
+#include <iostream>
+
+using namespace std;
 
 typedef std::queue<Node *> NodeQueue;
 
 Node::Node(void)
 {
+	Reset();
+}
+
+bool Node::IsAncestorOf(Node *pOther)
+{
+	return false;
+}
+
+void Node::Reset(void)
+{
 	m_bVisited = false;
+	m_EntryTime = -1;
+	m_ExitTime = -1;
 }
 
 Graph::Graph(void)
 {
-	m_pGraphClient = NULL;
+    m_pGraphClient = NULL;
+	m_nTime = 0;
 }
 
-void Graph::m_Visit(Node *pNode)
+void Graph::m_Visit(Node *pFromNode, Node *pToNode, int nTime)
 {
-	if(m_pGraphClient != NULL)
+	pToNode->m_EntryTime = nTime;
+    if(m_pGraphClient != NULL)
+    {
+            m_pGraphClient->Visit(pFromNode, pToNode);
+    }
+    pToNode->m_bVisited = true;
+}
+
+void Graph::m_ResetNodes(void)
+{
+	NodeVector::iterator nodesIterator = m_Nodes.begin();
+
+	for(; nodesIterator != m_Nodes.end(); ++nodesIterator)
 	{
-		m_pGraphClient->Visit(pNode);
+		Node *node = (*nodesIterator);
+		node->Reset();
 	}
-	pNode->m_bVisited = true;
+	m_nTime = 0;
 }
 
-void Graph::SetClient(GraphClient *pGraphClient)
+void Graph::SetClient(IGraphClient *pGraphClient)
 {
-	m_pGraphClient = pGraphClient;
+    m_pGraphClient = pGraphClient;
 }
 
 void Graph::AddNode(Node *pNode)
@@ -33,37 +62,62 @@ void Graph::AddNode(Node *pNode)
 	m_Nodes.push_back(pNode);
 }
 
+NodeVector Graph::GetNodes(void)
+{
+	return m_Nodes;
+}
+
+Node *Graph::GetNode(int position)
+{
+	return m_Nodes[position];
+}
+
 void Graph::DFIterate(Node *pNode)
 {
-	m_Visit(pNode);
-	
-	NodeList::iterator neighboursIterator = pNode->m_Neighbours.begin();
-	for(; neighboursIterator != pNode->m_Neighbours.end(); ++neighboursIterator)
-	{
+	m_ResetNodes();
+	if(pNode == NULL)
+		pNode = m_Nodes[0];
+	m_PerformDFIterate(NULL, pNode);
+}
+
+void Graph::m_PerformDFIterate(Node *pFromNode, Node *pToNode)
+{
+	m_Visit(pFromNode, pToNode, m_nTime);
+
+    NodeList::iterator neighboursIterator = pToNode->m_Neighbours.begin();
+
+    for(; neighboursIterator != pToNode->m_Neighbours.end(); ++neighboursIterator)
+    {
+		++m_nTime;
 		if(!(*neighboursIterator)->m_bVisited)
-		{
-			DFIterate(*neighboursIterator);
-		}
-	}
+        {
+			m_PerformDFIterate(pToNode, *neighboursIterator);
+        }
+    }
+
+	pToNode->m_ExitTime = m_nTime;
 }
 
 void Graph::BFIterate(Node *pNode)
 {
-	NodeQueue nodeQueue;
-	
-	m_Visit(pNode);
-	nodeQueue.push(pNode);
-	
-	while(!nodeQueue.empty())
-	{
-		Node *node = nodeQueue.front();
-		nodeQueue.pop();
-		NodeList::iterator nodeListIterator = node->m_Neighbours.begin();
-		
-		for(; (nodeListIterator != node->m_Neighbours.end()) && (!(*nodeListIterator)->m_bVisited); ++nodeListIterator)
-		{
-			m_Visit(*nodeListIterator);
-			nodeQueue.push(*nodeListIterator);
-		}
-	}
+    NodeQueue nodeQueue;
+    m_ResetNodes();
+
+	m_Visit(NULL, pNode, m_nTime);
+    nodeQueue.push(pNode);
+        
+	Node *pFromNode = pNode;
+
+    while(!nodeQueue.empty())
+    {
+        Node *node = nodeQueue.front();
+        nodeQueue.pop();
+        NodeList::iterator nodeListIterator = node->m_Neighbours.begin();
+                
+        for(; (nodeListIterator != node->m_Neighbours.end()) && (!(*nodeListIterator)->m_bVisited); ++nodeListIterator)
+        {
+                m_Visit(*nodeListIterator);
+                nodeQueue.push(*nodeListIterator);
+        }
+    }
 }
--- graph.h
+++ graph.h
@@ -5,36 +5,43 @@ class Node;
 
 typedef std::list<Node *> NodeList;
 typedef std::vector<Node *> NodeVector;
+typedef std::pair<int, int> Edge;
 
 class Node
 {
 public:
-	NodeList m_Neighbours;
-	bool m_bVisited;
-	int m_EntryTime, m_ExitTime;
-	
-	Node(void);
+        NodeList m_Neighbours;
+        bool m_bVisited;
+        int m_EntryTime, m_ExitTime;
+        
+        Node(void);
+		bool IsAncestorOf(Node *pOther);
+		void Reset(void);
 };
 
-class GraphClient
+class IGraphClient
 {
 public:
-	/* To be called before the node is set to visited */
-	virtual void Visit(Node *node) = 0;
+        /* To be called before the node is set to visited */
+        virtual void Visit(Node *pFromNode, Node *pToNode) = 0;
 };
 
 class Graph
 {
-	GraphClient *m_pGraphClient;
-	NodeVector m_Nodes;
-	
-	void m_Visit(Node *pNode);
-	
+        IGraphClient *m_pGraphClient;
+        NodeVector m_Nodes;
+        int m_nTime;
+
+        void m_Visit(Node *pFromNode, Node *pToNode, int nTime);
+        void m_ResetNodes(void);
+		void m_PerformDFIterate(Node *pFromNode, Node *pToNode);
 public:
-	Graph(void);
-	
-	void SetClient(GraphClient *pGraphClient);
-	void AddNode(Node *pNode);
-	void DFIterate(Node *pNode);
-	void BFIterate(Node *pNode);
+        Graph(void);
+        
+        void SetClient(IGraphClient *pGraphClient);
+        void AddNode(Node *pNode);
+        Node *GetNode(int position);
+        NodeVector GetNodes(void);
+        void DFIterate(Node *pNode);
+        void BFIterate(Node *pNode);
 };
